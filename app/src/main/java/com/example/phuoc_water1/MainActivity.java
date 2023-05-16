e56d49b16a9bebda0dc23188491fdd3899c02171
phuoc3004 <100799248+phuoc3004@users.noreply.github.com>
2023-05-16 20:24:15 +0700

    pdt

---
+++ app/src/main/java/com/example/phuoc_water1/MainActivity.java
@@ -0,0 +1,114 @@
+package com.example.phuoc_water1;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String ESP8266_IP_ADDRESS = "192.168.1.102";  // Replace with your ESP8266 IP address
+
+    private Button btnOn;
+    private Button btnOff;
+    private TextView tvSensorValue;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnOn = findViewById(R.id.btnOn);
+        btnOff = findViewById(R.id.btnOff);
+        tvSensorValue = findViewById(R.id.tvSensorValue);
+
+        btnOn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendRequest("/on");
+            }
+        });
+
+        btnOff.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendRequest("/off");
+            }
+        });
+
+        startSensorValueUpdates();
+    }
+
+    private void sendRequest(String endpoint) {
+        String url = "http://" + ESP8266_IP_ADDRESS + endpoint;
+        new HttpRequestTask().execute(url);
+    }
+
+    private void startSensorValueUpdates() {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (!Thread.currentThread().isInterrupted()) {
+                    try {
+                        Thread.sleep(1000);  // Update every 1 second
+                        String url = "http://" + ESP8266_IP_ADDRESS + "/";
+                        new HttpRequestTask().execute(url);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }).start();
+    }
+
+    private class HttpRequestTask extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected String doInBackground(String... params) {
+            if (params.length == 0)
+                return null;
+
+            String urlString = params[0];
+            StringBuilder result = new StringBuilder();
+
+            try {
+                URL url = new URL(urlString);
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                connection.setRequestMethod("GET");
+
+                BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    result.append(line);
+                }
+
+                reader.close();
+                connection.disconnect();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            return result.toString();
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            if (result != null) {
+                if (result.startsWith("Moisture Level:")) {
+                    String sensorValue = result.replace("Moisture Level:", "").trim();
+                    tvSensorValue.setText("Sensor Value: " + sensorValue);
+                }
+            }
+        }
+    }
+}
